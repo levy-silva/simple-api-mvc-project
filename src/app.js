79e1c3016c0aaa38e98d2aed319f031bc8f1a11a
Levy Silva <levysilva.dev@gmail.com>
2024-10-22 17:02:54 -0300

    chore: integrate router, database and errorHandler in app.js for application setup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const router = require("./routes/router");
+const errorHandler = require("./middlewares/errorHandler");
+
+require("./database/models/index");
 
 const app = express();
+router(app);
+
+app.use(errorHandler)
 
 module.exports = app;
